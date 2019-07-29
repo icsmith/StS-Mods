69900e315bb3535241891735604d7a0656b3eea0
icsmith <ianc.smith@live.com>
2019-07-29 01:26:20 -0500

    Aesthetic modifications

--- RoR_Engineer/src/main/java/RoR_Engineer/cards/OrbSkill.java
+++ RoR_Engineer/src/main/java/RoR_Engineer/cards/OrbSkill.java
@@ -25,7 +25,7 @@ public class OrbSkill extends AbstractDynamicCard {
     public static final String ID = RoR_Engineer.makeID(OrbSkill.class.getSimpleName());
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
 
-    public static final String IMG = makeCardPath("Skill.png");
+    public static final String IMG = makeCardPath("TR12_Gauss_Auto-Turret.png");
 
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;

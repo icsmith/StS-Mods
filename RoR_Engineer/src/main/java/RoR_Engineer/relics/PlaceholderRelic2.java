30ade0b5324665f4b17e907ecb6b5f79ebfb43d6
icsmith <ianc.smith@live.com>
2019-07-28 22:26:26 -0500

    Slay The Spire first commit. One day of work. Turret orb implemented.

---
+++ RoR_Engineer/src/main/java/RoR_Engineer/relics/PlaceholderRelic2.java
@@ -0,0 +1,56 @@
+package RoR_Engineer.relics;
+
+import basemod.abstracts.CustomRelic;
+import com.badlogic.gdx.graphics.Texture;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
+import com.megacrit.cardcrawl.actions.defect.RemoveNextOrbAction;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.powers.NextTurnBlockPower;
+import com.megacrit.cardcrawl.powers.StrengthPower;
+import RoR_Engineer.RoR_Engineer;
+import RoR_Engineer.util.TextureLoader;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
+
+import static RoR_Engineer.RoR_Engineer.makeRelicOutlinePath;
+import static RoR_Engineer.RoR_Engineer.makeRelicPath;
+
+public class PlaceholderRelic2 extends CustomRelic {
+    /*
+     * https://github.com/daviscook477/BaseMod/wiki/Custom-Relics
+     *
+     * At the start of each combat, gain 1 Strength (i.e. Vajra)
+     */
+
+    // ID, images, text.
+    public static final String ID = RoR_Engineer.makeID("PlaceholderRelic2");
+
+    private static final Texture IMG = TextureLoader.getTexture(makeRelicPath("placeholder_relic2.png"));
+    private static final Texture OUTLINE = TextureLoader.getTexture(makeRelicOutlinePath("placeholder_relic2.png"));
+
+    public PlaceholderRelic2() {
+        super(ID, IMG, OUTLINE, RelicTier.COMMON, LandingSound.FLAT);
+    }
+
+
+    // Gain 1 Strength on on equip.
+    @Override
+    public void atBattleStart() {
+        flash();
+        AbstractDungeon.actionManager.addToTop(new ApplyPowerAction(AbstractDungeon.player, AbstractDungeon.player, new StrengthPower(AbstractDungeon.player, 1), 1));
+        AbstractDungeon.actionManager.addToTop(new RelicAboveCreatureAction(AbstractDungeon.player, this));
+    }
+
+
+    // Description
+    @Override
+    public String getUpdatedDescription() {
+        return DESCRIPTIONS[0];
+    }
+
+    public void onLoseHp(int damageAmount) {
+        if (AbstractDungeon.getCurrRoom().phase == AbstractRoom.RoomPhase.COMBAT && damageAmount > 0) {
+            AbstractDungeon.actionManager.addToTop(new RemoveNextOrbAction());
+        }
+    }
+}
